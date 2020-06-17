851e55de6f4b838fbc78cce9d427ab604c15add2
Trayvon Banks <Trayvon.Banks@aa.com>
2020-06-17 11:58:15 -0500

    Implementing Search script

--- selenium_test.py
+++ selenium_test.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 
 # from selenium import webdriver
-base_url="https://www.amazon.in"
+base_url="https://www.amazon.com"
 # declare and initialize driver variable
 driver=webdriver.Chrome(executable_path=(r"C:\Users\241306\Documents\GitHub\Drivers\chromedriver.exe"))
 driver.get('http://google.com/')
@@ -15,5 +15,21 @@ driver.implicitly_wait(10) #10 is in seconds
 driver.get(base_url)
 # test whether correct URL/ Web Site has been loaded or not
 assert "Amazon" in driver.title
+
+
+# -- Steps --
+# to enter search term, we need to locate the search textbox
+searchTextBox=driver.find_element_by_id("twotabsearchtextbox")
+# to clear any text in the search textbox
+searchTextBox.clear()
+# to enter the search term in the search textbox via send_keys() function
+searchTextBox.send_keys(search_term)
+# to search for the entered search term
+searchTextBox.send_keys(Keys.RETURN)
+# to verify if the search results page loaded
+assert f"Amazon.in:{search_term}" in driver.title
+# to verify if the search results page contains any results or no results were found.
+assert "No results found." not in driver.page_source
+# -- Post - Condition --
 # to close the browser
 driver.close()
