09ff0a432b1d3e113deaf17cc628dabb17e40cf8
Trayvon Banks <trayvonbanks@aa.com>
2020-06-24 12:18:12 -0700

    base workspace

--- selenium_test.py
+++ selenium_test.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 
 # from selenium import webdriver
-base_url="https://www.amazon.com"
+base_url="https://www.cargurus.com"
 # declare and initialize driver variable
 driver=webdriver.Chrome(r"C:\Users\241306\Documents\GitHub\webscraping\chromedriver.exe")
 driver.get('http://google.com/')
@@ -14,10 +14,12 @@ driver.implicitly_wait(10) #10 is in seconds
 # to load a given URL in browser window
 driver.get(base_url)
 # test whether correct URL/ Web Site has been loaded or not
-assert "Amazon" in driver.title
+assert "cargurus" in driver.title
 
 
 # -- Steps --
+# "//*[@id="carPickerUsed_makerSelect"]""
+
 # to enter search term, we need to locate the search textbox
 searchTextBox=driver.find_element_by_id("twotabsearchtextbox")
 # to clear any text in the search textbox
@@ -27,9 +29,9 @@ searchTextBox.send_keys(search_term)
 # to search for the entered search term
 searchTextBox.send_keys(Keys.RETURN)
 # to verify if the search results page loaded
-assert f"Amazon.in:{search_term}" in driver.title
+assert f"Amazon.com:{search_term}" in driver.title
 # to verify if the search results page contains any results or no results were found.
 assert "No results found." not in driver.page_source
 # -- Post - Condition --
-# to close the browser
-driver.close()
+# # to close the browser
+# driver.close()
